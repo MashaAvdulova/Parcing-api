8f35b46a6b76895aae969084a8de6d72094fc73e
MashaAvdulova <avdulova@gmail.com>
2025-02-28 00:53:33 +0300

    27022025-2

--- genderize.py
+++ genderize.py
@@ -1,3 +1,5 @@
+from logging import getLevelName
+
 import requests
 
 # def get_data(url: str):
@@ -22,11 +24,22 @@ def get_data(url: str):
     return data
 
 def parse_data(data: dict):
-    name = data.get('name')
+    name = data.get('name').capitalize()
     gender = data.get('gender')
     probability = data.get('probability')
-    print(name,gender, probability)
+    if name.isalpha():
+        if gender == 'female':
+            gender_rus = 'женскому'
+        elif gender == 'male':
+            gender_rus = 'мужскому'
+        print(f'Имя {name} относится к {gender_rus} полу с вероятностью {probability: .0%}')
+    else:
+        print('Имя введено не корректно')
 
 name = input('Введите имя: ')
 data = get_data(name)
 parse_data(data)
+
+# Дописать программу таким образом, чтобы в случае корректного имени выводилось сообщение
+# в формате: Имя Elena относится к женскому полу с вероятностью 99%
+# В случае некорректного имени выводилось сообщение Имя введено не корректно.
